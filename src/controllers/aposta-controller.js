5488611bd4d7317acae765c9b61b757b26433b26
leachenski <jleachenski@gmail.com>
2024-09-18 21:37:51 -0300

    feat: jogo, aposta e bicho

---
+++ src/controllers/aposta-controller.js
@@ -0,0 +1,44 @@
+import Aposta from "../models/aposta-model.js";
+
+async function store(req, res) {
+  try {
+    await Aposta.create(req.body);
+    res.json();
+  } catch (error) {
+    res.status(400).json(error);
+  }
+}
+
+async function index(req, res) {
+  try {
+    const content = await Aposta.find(req.query).exec();
+    res.json(content);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+}
+
+async function show(req, res) {
+  try {
+    const content = await Aposta.findById(req.params.id).exec();
+    res.json(content);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+}
+
+async function update(req, res) {
+  try {
+    await Aposta.findByIdAndUpdate(req.params.id, req.body).exec();
+    res.json();
+  } catch (error) {
+    res.status(400).json(error);
+  }
+}
+
+export default {
+  store,
+  index,
+  show,
+  update,
+};
